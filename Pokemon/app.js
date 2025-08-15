c192aa7ec3c065a07dce043f4c34fd4decf92ea9
Sudharsank05 <sudharspoy@gmail.com>
2025-08-15 23:17:01 +0530

    css changes, alert added - pika pika...

--- Pokemon/app.js
+++ Pokemon/app.js
@@ -1,19 +1,39 @@
-// https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png
-
 const container = document.querySelector('#container');
-const baseURL = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
+const baseURL = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
 
+async function loadPokemon() {
+    const promises = [];
 
-for (let i = 1; i <= 151; i++) {
-    const pokemon = document.createElement('div');
-    pokemon.classList.add('pokemon');
-    const label = document.createElement('span');
-    label.innerText = `#${i}`;
-    const newImg = document.createElement('img');
-    newImg.src = `${baseURL}${i}.png`
+    for (let i = 1; i <= 151; i++) {
+        promises.push(fetch(`https://pokeapi.co/api/v2/pokemon/${i}`).then(res => res.json()));
+    }
 
+    const allPokemon = await Promise.all(promises);
 
-    pokemon.appendChild(newImg);
-    pokemon.appendChild(label);
-    container.appendChild(pokemon)
+    allPokemon.forEach((data, index) => {
+        const i = index + 1;
+
+        const pokemon = document.createElement('div');
+        pokemon.classList.add('pokemon');
+
+        const label = document.createElement('span');
+        label.innerText = `#${i}`;
+
+        const nameLabel = document.createElement('p');
+        nameLabel.innerText = data.forms[0].name;
+
+        const newImg = document.createElement('img');
+        newImg.src = `${baseURL}${i}.png`;
+
+        pokemon.appendChild(newImg);
+        pokemon.appendChild(label);
+        pokemon.appendChild(nameLabel);
+        container.appendChild(pokemon);
+
+        pokemon.addEventListener('click', () => {
+            alert(`You clicked ${nameLabel.innerText}`);
+        });
+    });
 }
+
+loadPokemon();
